246840dbe6333d7079b1255117159d1e6c6e7aef
laurence <laurence.jones@live.co.uk>
2023-02-07 19:25:44 +0000

    beautify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,21 +1,36 @@
-import { createApp, h } from 'vue'
+import {
+  createApp,
+  h
+} from 'vue'
 import './index.css'
 import App from './App.vue'
 import '/src/assets/wasm_exec.js'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core'
 
 /* import font awesome icon component */
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faClipboard, faGear, faQuestion, faXmark } from '@fortawesome/free-solid-svg-icons'
+import {
+  faClipboard,
+  faGear,
+  faQuestion,
+  faXmark
+} from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
 library.add(faGear, faQuestion, faXmark, faClipboard)
 
 const go = new Go();
-WebAssembly.instantiateStreaming(fetch(import.meta.env.PROD ? '/assets/ctt.wasm' : '/src/assets/ctt.wasm'), go.importObject).then(({ instance }) => {
+WebAssembly.instantiateStreaming(fetch(
+  import.meta.env.PROD ? '/assets/ctt.wasm' : '/src/assets/ctt.wasm'), go.importObject).then(({
+  instance
+}) => {
   go.run(instance);
   createApp(App).use({
     install(app) {
@@ -25,12 +40,12 @@ WebAssembly.instantiateStreaming(fetch(import.meta.env.PROD ? '/assets/ctt.wasm'
         const container = document.createElement('div')
         document.body.appendChild(container)
         let toastApp = createApp({
-          render () {
+          render() {
             return h("div", {
-            class: "fixed bottom-0 right-0 m-4",
-          }, [h("div", {
-            class: `${opts.error ? `bg-red-500` : `bg-blue-500`} px-4 py-2 rounded-xl`
-          }, message)])
+              class: "fixed bottom-0 right-0 m-4",
+            }, [h("div", {
+              class: `${opts.error ? `bg-red-500` : `bg-blue-500`} px-4 py-2 rounded-xl`
+            }, message)])
           }
         })
         toastApp.mount(container)
